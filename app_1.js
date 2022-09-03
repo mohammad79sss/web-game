6fa9e92788b0ad87bad17ca6d13a772500c8b500
mohammad <mohammad79std@gmail.com>
2022-09-03 22:06:45 +0430

    add some animation

--- app_1.js
+++ app_1.js
@@ -1,17 +1,17 @@
 
 function jump(){
     document.getElementById("character").id = "jumpedCharacter";
-    setTimeout(reverse, 200);
+    setTimeout(reverse, 2000);
 }
 function reverse(){
     document.getElementById("jumpedCharacter").id = "character";
 }
 
-
 var upto=0;
 var score= document.getElementById("scn");
-setInterval(up,550)
+setInterval(up,350)
 function up(){
     score.innerHTML=upto;
-    upto+=10;
+    upto+=7;
 }
+
